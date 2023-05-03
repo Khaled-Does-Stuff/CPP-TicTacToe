5b3fc4581abda478c6f8cb6eefd61aac04428b54
Khaled <spkhalad@gmail.com>
2023-05-03 12:39:32 +0600

    Added all win / draw logic
    Game now works

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,6 +6,10 @@ void takeInput();
 bool gameOver();
 string getWinPlayer();
 
+inline bool theyAreSame(string x, string y, string z) {
+    return (x == y && y == z);
+}
+
 string gameBoard[9] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 bool playerOne = true;
 
@@ -15,23 +19,28 @@ int main() {
         takeInput();
     } while (getWinPlayer() == "");
 
-    cout << endl
-         << getWinPlayer() << " Won";
+    printCurrentGameState();
+    cout
+        << endl
+        << getWinPlayer() << " Won";
 
     return 0;
 }
 
 void printCurrentGameState() {
+    // TODO: Hard to read
+
+    cout << endl;
     cout << gameBoard[0] << " | " << gameBoard[1] << " | " << gameBoard[2] << endl;
     cout << "---------" << endl;
     cout << gameBoard[3] << " | " << gameBoard[4] << " | " << gameBoard[5] << endl;
     cout << "_________" << endl;
     cout << gameBoard[6] << " | " << gameBoard[7] << " | " << gameBoard[8] << endl;
+    cout << endl;
 }
 
 void takeInput() {
-    cout << endl
-         << "Please give input: (Example: 3 or 1), Player "
+    cout << "Please give input: (Example: 3 or 1), Player "
          << (playerOne ? "X" : "O")
          << ": ";
 
@@ -45,8 +54,33 @@ void takeInput() {
 }
 
 string getWinPlayer() {
-    if ((gameBoard[0] == gameBoard[1]) && (gameBoard[1] == gameBoard[2]))
+    // Horizontal checks
+    if (theyAreSame(gameBoard[0], gameBoard[1], gameBoard[2]))
         return gameBoard[0];
+
+    if (theyAreSame(gameBoard[3], gameBoard[4], gameBoard[5]))
+        return gameBoard[3];
+
+    if (theyAreSame(gameBoard[6], gameBoard[7], gameBoard[8]))
+        return gameBoard[6];
+
+    // Vertical checks
+    if (theyAreSame(gameBoard[0], gameBoard[3], gameBoard[6]))
+        return gameBoard[0];
+
+    if (theyAreSame(gameBoard[1], gameBoard[4], gameBoard[7]))
+        return gameBoard[1];
+
+    if (theyAreSame(gameBoard[2], gameBoard[5], gameBoard[8]))
+        return gameBoard[2];
+
+    // Corner checks
+
+    if (theyAreSame(gameBoard[0], gameBoard[4], gameBoard[8]))
+        return gameBoard[0];
+
+    if (theyAreSame(gameBoard[2], gameBoard[4], gameBoard[6]))
+        return gameBoard[2];
 
     for (string val : gameBoard) {
         if (val != "X" && val != "O") return "";
